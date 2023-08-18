44bbed255d2449a76932033065ff3177f75e619e
southernMD <2483723241@qq.com>
2023-08-18 23:24:11 +0800

    add one line

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -8,7 +8,7 @@ useColor()
 </script>
 
 <template>
-<div class="contain" v-if="!globalVar.radioReady">大牛马音乐</div>
+<div class="contain" v-if="!globalVar.radioReady && $route.path.includes('/app')">大牛马音乐</div>
 <Suspense>
     <template #default>
         <RouterView></RouterView>
